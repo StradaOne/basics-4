230825945774a84103a023a8d01dc582cc6d04a6
Anastasia <tgmonresu@gmail.com>
2023-04-03 21:49:17 +0300

    v6. Ideal version

--- monresu/main.js
+++ monresu/main.js
@@ -1,59 +1,76 @@
-const errorNotTask = 'Sorry! Такой задачи нет!'
-const errorIsTask = 'Sorry! Такая задача уже в наличии'
-const deleteTaskConst = 'Complete! Task was удалена'
-const addTaskConst = 'Task was добавлена'
-const changeTaskConst = 'Task was изменена'
+const message = {
+  errorNotTask: 'Sorry! Такой задачи нет!',
+  errorIsTask: 'Sorry! Такая задача уже в наличии',
+  deleteTaskConst: 'Complete! Task was удалена',
+  addTaskConst: 'Task was добавлена',
+  changeTaskConst: 'Task was изменена',
+  errorTaskIsEmpty: 'Task пустая',
+}
+
+const STATUS = {
+  TODO: 'todo',
+  IN_PROGRESS: 'in progress',
+  DONE: 'done',
+  TEST: 'test',
+}
+
+function isTaskInList(task) {
+  return task in todoList.todos;
+}
+
+function isTaskTextValid(task) {
+  return !task;
+}
 
 const todoList = {
-	todos: {
-  },
+	todos: {},
   changeStatus(task, text) {
-    if (task in this.todos == false) {
-      console.log(errorNotTask)
+    if (!isTaskInList(task)) {
+      console.log(message.errorNotTask)
+      return;
     }
-    else {
-      this.todos[task] = text;
-      console.log(changeTaskConst)
-    }
+    this.todos[task] = text;
+    console.log(message.changeTaskConst)
   },
-  addTask(task, status = 'todo') {
-    if (task in this.todos == true) {
-      console.log(errorIsTask)
+  addTask(task, status = STATUS.TODO) {
+    if (isTaskInList(task)) {
+      console.log(message.errorIsTask, task)
+      return;
     }
-    else {
-      this.todos[task] = status;
-      console.log(addTaskConst)
+    if (isTaskTextValid(task)) {
+      console.log(message.errorTaskIsEmpty);
+      return;
     }
+    this.todos[task] = status;
+    console.log(message.addTaskConst)
   },
   deleteTask(task) {
-    if (task in this.todos == false) {
-      console.log(errorNotTask)
+    if (!isTaskInList(task)) {
+      console.log(message.errorNotTask)
+      return;
     }
-    else {
-      delete this.todos[task];
-      console.log(deleteTaskConst)
-    }
-  },
-  displayStatus(status) {
-    let tasks = '';
-    for (const key in this.todos) {
-      if (this.todos[key].toLowerCase() === status.toLowerCase()) {
-        tasks += `\n\t${key}`
-      }
-    }
-    console.log(status + ':', tasks ? tasks : '\n\t-');
+    delete this.todos[task];
+    console.log(message.deleteTaskConst)
   },
   showList() {
-    this.displayStatus('Todo');
-    this.displayStatus('Done');
-    this.displayStatus('In Progress');
+    for (const stat in STATUS) {
+      let tasks = '';
+      for (const key in this.todos) {
+        if (this.todos[key].toLowerCase() === STATUS[stat].toLowerCase()) {
+          tasks += `\n\t${key}`
+        }
+      }
+      console.log(`--${STATUS[stat].toUpperCase()}--`, tasks ? tasks : '\n\tNothing tasks!');
+    }
   }
 }
 
 todoList.addTask('have a walk');
 todoList.addTask('SLEEPY');
 todoList.addTask('delete JSON from web');
+todoList.addTask('TESTIKIIGIIGIGIG', STATUS.TEST);
+todoList.addTask('');
 todoList.deleteTask('drink tea');
-todoList.changeStatus('delete JSON from web', 'Done');
-todoList.changeStatus('SLEEPY', 'in progress');
+todoList.changeStatus('delete JSON from web', STATUS.DONE);
+todoList.changeStatus('SLEEPY', STATUS.IN_PROGRESS);
 todoList.showList()
