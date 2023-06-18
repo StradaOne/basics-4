1c221a2a8e59d6d92e14175ff7860bf923674854
Anastasia <tgmonresu@gmail.com>
2023-06-18 14:20:06 +0300

    change type + undef check

--- monresu/main.js
+++ monresu/main.js
@@ -1,26 +1,32 @@
 'use strict'
 
 class Storage {
-  constructor(key, value = '') {
+  constructor(key, value = '', typeStorage = 'localStorage') {
     this.key = key;
-    localStorage.setItem(this.key, value);
+    this.storage = typeStorage === 'localStorage' ? localStorage : sessionStorage;
+    this.storage.setItem(this.key, value);
   };
   get() {
-    return localStorage.getItem(this.key);
+    return this.storage.getItem(this.key);
   };
   set(value) {
-    localStorage.setItem(this.key,(value));
+    this.storage.setItem(this.key,(value));
   };
   clear() {
-    localStorage.removeItem(this.key);
+    this.storage.removeItem(this.key);
   };
   isEmpty() {
-    return !localStorage.getItem(this.key);
+    const item = this.storage.getItem(this.key);
+    return item === 'undefined' || item === null;
   };
 };
 
-const names = new Storage('names');
-names.set('John');
+const STORAGE_NAMES = {
+  localStorage: 'localStorage',
+  sessionStorage: 'sessionStorage',
+}
+
+const names = new Storage('names', '123', STORAGE_NAMES.sessionStorage);
 console.log(names.get()); // 'John'
 console.log(names.isEmpty()); // false
 
@@ -30,5 +36,6 @@ console.log(age.get()); // 25
 console.log(age.isEmpty()); // false
 
 names.clear();
+names.set();
 console.log(names.get()); // null
 console.log(names.isEmpty()); // true
