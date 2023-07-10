2849094f47c07674f467f817aeea5c625c151d11
cloudeqv <vladvoronin428@gmail.com>
2023-07-10 21:17:36 +0500

    init commit

---
+++ weitend/src/JS/ui.mjs
@@ -0,0 +1,19 @@
+const ELEMENTS = {
+    CHAT: document.querySelector('.chat'),
+    SEND: {
+        FORM: document.querySelector('.send__form'),
+        INPUT: document.querySelector('.send__form-input'),
+        BTN: document.querySelector('.send__form-btn'),
+    },
+    EXTRA: {
+        MSG_TEMPLATE: document.querySelector('#msgTemplate'),
+        AUTHORIZATION_FORM: document.querySelector('#authorizationForm'),
+        AUTHORIZATION_INPUT: document.querySelector('#authorizationInput'),
+        AUTHORIZATION_BTN: document.querySelector('#authorizationBtn'),
+        CONFIRM_FORM: document.querySelector('#confirmForm'),
+        CONFIRM_INPUT: document.querySelector('#confirmInput'),
+        CONFIRM_BTN: document.querySelector('#confirmBtn'),
+    },
+};
+
+export { ELEMENTS };
