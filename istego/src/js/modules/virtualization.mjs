ee60a9df3b55444afdd3b9e79356b1cfb7eb4843
Ivan Marahovskii <marahovskii81@gmail.com>
2023-07-11 22:07:27 +0500

    fix scroll

--- istego/src/js/modules/virtualization.mjs
+++ istego/src/js/modules/virtualization.mjs
@@ -21,10 +21,14 @@ function limitMessages(arrayHistoryMessages, limitCount) {
     }
 }
 
+
 function virtualScrollMessages() {
     if (UI.dialogWindow.scrollTop === 0) {
+        
         limitMessages(getStorage('messages'), 20);
-        UI.dialogWindow.scrollTop = 50;
+        
+        UI.dialogWindow.scrollTop = 1394;
+        
     }
 }
 
