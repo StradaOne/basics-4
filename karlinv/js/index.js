cc7f69b29398b5d9402e21ae3f0ffdf55e9bd439
Владимир Карлин <vow.carlin@yandex.ru>
2023-07-12 14:01:59 +0300

    deleting folders

--- karlinv/js/index.js
+++ karlinv/js/index.js
@@ -1,30 +1,30 @@
-"use strict";
+'use strict';
 function add(num) {
-    return num.a + num.b;
+	return num.a + num.b;
 }
 function subtract(num) {
-    return num.a - num.b;
+	return num.a - num.b;
 }
 function multiply(num) {
-    return num.a * num.b;
+	return num.a * num.b;
 }
 function divide(num) {
-    if (!num.b) {
-        throw new Error('Деление на ноль не допускается.');
-    }
-    return num.a / num.b;
+	if (!num.b) {
+		throw new Error('Деление на ноль не допускается.');
+	}
+	return num.a / num.b;
 }
 function calc(a, operation, b) {
-    switch (operation) {
-        case '+':
-            return add({ a: a, b: b });
-        case '-':
-            return subtract({ a: a, b: b });
-        case '*':
-            return multiply({ a: a, b: b });
-        case '/':
-            return divide({ a: a, b: b });
-        default:
-            throw new Error(`Не верное значение${operation}, используйте [+, -, *, /]`);
-    }
+	switch (operation) {
+		case '+':
+			return add({ a: a, b: b });
+		case '-':
+			return subtract({ a: a, b: b });
+		case '*':
+			return multiply({ a: a, b: b });
+		case '/':
+			return divide({ a: a, b: b });
+		default:
+			throw new Error(`Не верное значение${operation}, используйте [+, -, *, /]`);
+	}
 }
